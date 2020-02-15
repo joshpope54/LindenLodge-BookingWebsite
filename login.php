d05f1014e521a81d7ff4eb50befb2ae0e8e27b45
Ben R F <benramsayfoster@gmail.com>
2020-02-15 00:56:03 +0000

    Styled initial version of login page

--- login.php
+++ login.php
@@ -3,13 +3,17 @@
     <head>
         <meta charset="utf-8">
         <title>Linden lodge kennels</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato|Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="./styles/Login.css">
     </head>
     <body>
         <div class="login-box">
-
+            <img src="./images/logo.png" class="login-logo"/>
             <div class="login-form">
-                <input type="text" class="login-field"/>
-                <input type="text" class="login-field"/>
+                <input type="text" class="login-field" placeholder="USERNAME"/>
+                <br>
+                <input type="password" class="login-field" placeholder="PASSWORD"/>
+                <br>
                 <button class="login-btn" type="button"><span>Login</span></button>
             </div>
         </div>
